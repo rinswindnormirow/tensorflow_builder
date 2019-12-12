faa4c574f0c68fa1ff2d4cf5a81e9f80ad781845
wdm <wdm@wdm>
2019-12-12 16:23:57 +0700

    - runing configure script - running build

--- tensorflow_build.py
+++ tensorflow_build.py
@@ -5,6 +5,9 @@ from os import path
 import argparse
 import subprocess
 
+from shutil import which
+from functools import partial
+
 min_tf_version = '1.11'
 max_tf_version = '1.15'
 
@@ -88,44 +91,110 @@ def get_bazel(version):
     bazel_url = 'https://github.com/bazelbuild/bazel/releases/download/{0}/bazel-{0}-installer-linux-x86_64.sh'.format(version)
     bazel_dir = 'bazel_{}'.format(version)
 
-    os.system('rm -rf ./' + bazel_dir)
+    # os.system('rm -rf ./' + bazel_dir)
+    os.system('rm -rf ./bazel/bin')
 
     command = 'wget -P' + ' ' + bazel_dir + ' ' + bazel_url
-    subprocess.run([command], shell=True, check=True)
+    # subprocess.run([command], shell=True, check=True)
 
     current_dir = os.getcwd()
 
-    # os.system('cd ./{}'.format(bazel_dir))
-    os.system('chmod +x ./{0}/bazel-{1}-installer-linux-x86_64.sh'.format(bazel_dir, version))
-    os.system('mkdir ./{0}/bin'.format(bazel_dir))
-    # os.system('mkdir ./{0}/bin/bazel'.format(bazel_dir))
-    os.system('mkdir ./{0}/lib/'.format(bazel_dir))
-    os.system('mkdir ./{0}/lib/bazel'.format(bazel_dir))
+    os.system('chmod +x ./{0}/bazel-{1}-installer-linux-x86_64.sh'.format(bazel_dir, version))        #
+    os.system('mkdir ./{0}/bin'.format(bazel_dir))                                                    # ${BAZEL_DIR}/bin
+    os.system('mkdir ./{0}/lib/'.format(bazel_dir))                                                   # ${BAZEL_DIR}/lib
+    os.system('mkdir ./{0}/lib/bazel'.format(bazel_dir))                                              # ${BAZEL_DIR}/lib/bazel
 
-    bzl_install_command = './{1}/bazel-{0}-installer-linux-x86_64.sh --prefix={2}/{1}'.format(version, bazel_dir, current_dir)
+    os.system('mkdir ./bazel')
+    os.system('mkdir ./bazel/bin')
+
+    # bzl_install_command = './{1}/bazel-{0}-installer-linux-x86_64.sh --prefix={2}/{1}'.format(version, bazel_dir, current_dir)
+    bzl_install_command = './{1}/bazel-{0}-installer-linux-x86_64.sh --prefix={2}/{1}'.format(version, bazel_dir,
+                                                                                              current_dir)
     subprocess.run([bzl_install_command], shell=True, check=True)
+
+    # add path to the bazel to $PATH
+    old_path = os.environ['PATH']
+    # new_path = old_path + ':' + current_dir + '/' + bazel_dir + '/bin'
+    new_path = old_path + ':' + current_dir + '/bazel/bin'
+
+    os.environ['PATH'] = new_path
+    print(os.environ['PATH'])
+    print(which('bazel'))
+
+
+    os.system('ln -s {1}/{0}/lib/bazel/bin/bazel {1}/bazel/bin/'.format(bazel_dir, current_dir))
+
     return current_dir + '/{}'.format(bazel_dir) + '/bin'
 
 
 def tf_configure(tf_path, python_location, python_library_location, apache_ignite_support, XLAJIT, opencl, rocm, cuda, cuda_version,
-                 cuda_location):
+                 cuda_location, TensorRT, clang, mpi, opt_flag, android_wpc):
 
-    command = tf_path + '/configure'
+    def __stdin_write(child, str2write):
+        child.stdout.flush()
+        child.stdin.write(str2write + '\n')
+        child.stdin.flush()
 
-    configure_proc = subprocess.Popen([command], stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+    print(tf_path)
+    os.chdir('./' + tf_path)
+
+    # command = './' + tf_path + '/configure'
+    # os.system('chmod +x ' + command)
+
+    command = './configure'
+    os.system('chmod +x ' + command)
+
+    configure_proc = subprocess.Popen([command], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
 
     # poll check if child process has terminated
     while configure_proc.poll() is None:
-        line = configure_proc.stdout.readline()
+        line = ''
+        for c in iter(partial(configure_proc.stdout.read, 1), ''):
+            if c == '\n' or c == ':':
+                break
+            line += c
+
         print(line)
         if 'location of python' in line:
-            configure_proc.stdin.write(python_location + '\\n')
-        elif 'Python library path' in line:
-            pass
-        pass
+            __stdin_write(configure_proc, python_location)
+        elif 'desired Python library path' in line:
+            __stdin_write(configure_proc, python_library_location)
+        elif 'XLA JIT support? [Y/n]' in line:
+            __stdin_write(configure_proc, XLAJIT)
+        elif 'Apache Ignite Support' in line:
+            __stdin_write(configure_proc, apache_ignite_support)
+        elif 'OpenCL SYCL support? [y/N]' in line:
+            __stdin_write(configure_proc, opencl)
+        elif 'ROCm support? [y/N]' in line:
+            __stdin_write(configure_proc, rocm)
+        elif 'CUDA support? [y/N]' in line:
+            __stdin_write(configure_proc, cuda)
+        elif 'Do you wish to build TensorFlow with TensorRT support? [y/N]' in line:
+            __stdin_write(configure_proc, TensorRT)
+        elif 'fresh release of clang? (Experimental) [y/N]' in line:
+            __stdin_write(configure_proc, clang)
+        elif 'TensorFlow with MPI support? [y/N]' in line:
+            __stdin_write(configure_proc, mpi)
+        elif 'specify optimization flags' in line:
+            __stdin_write(configure_proc, opt_flag)
+        elif 'Android builds? [y/N]' in line:
+            __stdin_write(configure_proc, android_wpc)
+
+
+def tf_build(tf_dir):
+    bazel = which('bazel')
+    command = '{0} build --config=opt //tensorflow:libtensorflow_cc.so'.format(bazel)
+
+    # print(tf_dir)
+    # os.chdir('./' + tf_dir)
+
+    subprocess.run([command], shell=True, check=True)
 
 
 def main():
+
+    # print(os.getenv("PATH"))
+    # print(which('bazel'))
 
     parser = argparse.ArgumentParser(description='tensorflow 1.xx build and install')
     parser.add_argument("--t", default='1.12', help='tensorflow version')
@@ -141,6 +210,11 @@ def main():
     parser.add_argument("--CUDA", default='y')
     parser.add_argument("--CUDA-VERSION", default='')
     parser.add_argument("--CUDA-toolkit-location", default='')
+    parser.add_argument("--TensorRT-support", default='n')
+    parser.add_argument("--download-clang", default='n')
+    parser.add_argument("--MPI-support", default='n')
+    parser.add_argument("--opt-flag", default='-Wno-sign-compare')
+    parser.add_argument("--android-workspace", default='n')
 
     args = parser.parse_args()
     version = args.t
@@ -156,6 +230,11 @@ def main():
     CUDA = args.CUDA
     CUDA_VERSION = args.CUDA_VERSION
     CUDA_toolkit_location = args.CUDA_toolkit_location
+    TensorRT = args.TensorRT_support
+    clang = args.download_clang
+    mpi = args.MPI_support
+    opt_flag = args.opt_flag
+    android_wpc = args.android_workspace
 
     min_v = version_to_int(min_tf_version)
     max_v = version_to_int(max_tf_version)
@@ -174,6 +253,9 @@ def main():
     print(bzl_path)
 
     tf_configure(tf_path, python_location, python_library_location, apache_ignite_support,
-                 XLA_JIT, opencl_sycl, rocm, CUDA, CUDA_VERSION, CUDA_toolkit_location);
+                 XLA_JIT, opencl_sycl, rocm, CUDA, CUDA_VERSION, CUDA_toolkit_location,
+                 TensorRT, clang, mpi, opt_flag, android_wpc)
+
+    tf_build(tf_path)
 
 main()
